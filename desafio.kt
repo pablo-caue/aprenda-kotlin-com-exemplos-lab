e428a8b23b512e3331333b5ea90ac368c3747e5e
pablo-caue <pablocrs2004@gmail.com>
2023-08-20 12:04:24 -0300

    feat: update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,40 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String,val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} matriculado")
+    }
+    fun getMatricular(){
+        println("Alunos matriculados:")
+        for (i in inscritos){
+            println(i.nome)
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val listConteudo = listOf(
+        ConteudoEducacional("kotlin", 50),
+        ConteudoEducacional("javascript"),
+        ConteudoEducacional("java", 90))
+
+    val formacao = Formacao("Desenvolvedor", Nivel.DIFICIL, listConteudo)
+
+    formacao.matricular(Usuario("Marcos"))
+    formacao.matricular(Usuario("Eduardo"))
+    formacao.matricular(Usuario("Joao"))
+    formacao.matricular(Usuario("Pablo"))
+    formacao.matricular(Usuario("Davi"))
+    formacao.matricular(Usuario("André"))
+
+    formacao.getMatricular()
 }
